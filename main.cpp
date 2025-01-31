bca72194beb6abb2acb3e0fda233ac19e581ee64
Frank Krick <frank.krick@gmail.com>
2025-01-31 10:35:31 -0500

    print some port info

--- main.cpp
+++ main.cpp
@@ -1,46 +1,8 @@
-#include "glib-object.h"
+#include <glib-object.h>
 #include <iostream>
-#include <string>
+#include <pipewire/keys.h>
 #include <thread>
-#include <vector>
 #include <wp/wp.h>
-
-struct Port {
-  std::string alias;
-};
-
-std::vector<Port> iterate(WpObjectManager *om) {
-  std::vector<Port> result;
-  const auto om_iter = wp_object_manager_new_iterator(om);
-  GValue port_value;
-  while (wp_iterator_next(om_iter, &port_value)) {
-    /*
-    if (port_value.g_type != G_TYPE_OBJECT) {
-      std::cout << "Not an object" << std::endl;
-    }
-
-    if (port_value.g_type != WP_TYPE_PORT) {
-      std::cout << "Not a port" << std::endl;
-    }
-    */
-
-    std::cout << g_type_name(port_value.g_type) << std::endl;
-    /*
-        auto object = g_value_get_object(&port_value);
-        std::cout << "object: " << object << std::endl;
-        if (object == nullptr) {
-          continue;
-        }
-
-        WpPort *port = WP_PORT(object);
-        const auto property_value = wp_pipewire_object_get_property(
-            reinterpret_cast<WpPipewireObject *>(port), "port.alias");
-        result.push_back(Port{.alias = std::string(property_value)});
-        */
-    g_value_unset(&port_value);
-  }
-  return result;
-}
 
 struct WirePlumberControl {
   GMainLoop *loop = nullptr;
@@ -66,20 +28,22 @@ int main(int argc, char *argv[]) {
                                    wire_plumber_service.om);
 
     g_signal_connect(
-        wire_plumber_service.om, "installed",
-        G_CALLBACK(+[](WpObjectManager *object_manager, gpointer user_data) {
-          iterate(object_manager);
+        wire_plumber_service.om, "object-added",
+        G_CALLBACK(+[](WpObjectManager *object_manager, gpointer object,
+                       gpointer user_data) {
+          const auto g_object = static_cast<WpPipewireObject *>(object);
+
+          const auto id = std::atoi(
+              wp_pipewire_object_get_property(g_object, PW_KEY_PORT_ID));
+          const auto name = std::string(
+              wp_pipewire_object_get_property(g_object, PW_KEY_PORT_NAME));
+          const auto alias = std::string(
+              wp_pipewire_object_get_property(g_object, PW_KEY_PORT_ALIAS));
+
+          std::cout << "Object added, id " << id << ", name " << name
+                    << ", alias " << alias << std::endl;
         }),
         nullptr);
-
-    std::thread invoker([&wire_plumber_service]() {
-      std::this_thread::sleep_for(std::chrono::seconds(10));
-      iterate(wire_plumber_service.om);
-      std::cout << "Invoked" << std::endl;
-
-      iterate(wire_plumber_service.om);
-      std::cout << "Invoked" << std::endl;
-    });
 
     g_main_loop_run(wire_plumber_service.loop);
   }
